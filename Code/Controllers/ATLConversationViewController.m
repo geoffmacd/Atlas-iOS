53015d2beae826d1e7b86b97b1516533608b4fc4
Geoff MacDonald <geoff@imgur.com>
2016-12-13 14:55:11 -0800

    fixes another case from https://github.com/layerhq/Atlas-iOS/commit/5afe4e4e6c7543df71bb1a47027747a070035ead

--- Code/Controllers/ATLConversationViewController.m
+++ Code/Controllers/ATLConversationViewController.m
@@ -1210,7 +1210,6 @@ static NSInteger const ATLPhotoActionSheet = 1000;
            newIndexPath:(NSIndexPath *)newIndexPath
 {
     if (self.collectionView.window == nil) return;
-    if (self.expandingPaginationWindow) return;
     NSInteger currentIndex = indexPath ? [self.conversationDataSource collectionViewSectionForQueryControllerRow:indexPath.row] : NSNotFound;
     NSInteger newIndex = newIndexPath ? [self.conversationDataSource collectionViewSectionForQueryControllerRow:newIndexPath.row] : NSNotFound;
     [self.objectChanges addObject:[ATLDataSourceChange changeObjectWithType:type newIndex:newIndex currentIndex:currentIndex]];
